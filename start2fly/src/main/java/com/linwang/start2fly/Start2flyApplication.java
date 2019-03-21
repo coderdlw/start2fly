3fd02ff02a5392ccd62bd619f9ba51bb37787468
linwang <coderdlw@163.com>
2019-03-21 21:23:56 +0800

    新的开始

---
+++ start2fly/src/main/java/com/linwang/start2fly/Start2flyApplication.java
@@ -0,0 +1,13 @@
+package com.linwang.start2fly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Start2flyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Start2flyApplication.class, args);
+	}
+
+}
